5c5f266aa8557136d948e6c7b61bbf397a8428bf
sonofmagic <1324318532@qq.com>
2023-04-09 20:28:14 +0800

    chore: commit tw versions

--- packages/tailwindcss-patch/rollup.config.mjs
+++ packages/tailwindcss-patch/rollup.config.mjs
@@ -1,3 +1,15 @@
-import { createRollupConfig } from '@icebreakers/rollup'
+import { createRollupConfig, legacyOutputOptions } from '@icebreakers/rollup'
 
-export default createRollupConfig({})
+export default createRollupConfig({
+  input: {
+    index: 'src/index.ts',
+    cli: 'src/cli.ts'
+  },
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs',
+      ...legacyOutputOptions
+    }
+  ]
+})
