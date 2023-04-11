5c835794a5c0bf1a3f157ed795a262f69193a224
sonofmagic <1324318532@qq.com>
2023-04-12 00:09:32 +0800

    feat: babel to esm

--- packages/unplugin-tailwindcss-mangle/src/lib/babel.ts
+++ packages/unplugin-tailwindcss-mangle/src/lib/babel.ts
@@ -1,5 +1,9 @@
-import generate from '@babel/generator'
+import _generate from '@babel/generator'
 import { parse } from '@babel/parser'
-import traverse from '@babel/traverse'
+import _traverse from '@babel/traverse'
+
+const generate = (_generate as any).default as typeof _generate
+
+const traverse = (_traverse as any).default as typeof _traverse
 
 export { generate, parse, traverse }
