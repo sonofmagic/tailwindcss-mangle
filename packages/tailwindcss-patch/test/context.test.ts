ceccac451ff71fcfb23f105751c123c0795c6398
SonOfMagic <qq1324318532@gmail.com>
2023-04-17 01:46:11 +0800

    test: commit image url case

--- packages/tailwindcss-patch/test/context.test.ts
+++ packages/tailwindcss-patch/test/context.test.ts
@@ -44,4 +44,16 @@ describe('common usage', () => {
     expect(set.size).toBe(5)
     expect(result).toMatchSnapshot()
   })
+
+  // https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/158
+  it("bg-[url('img_src')] lose efficacy", () => {
+    getCss([getTestCase('img-url.jsx')])
+    const ctxs = getContexts()
+    expect(ctxs).toBeTruthy()
+    const set = getClassCacheSet()
+    expect(set.size).toBeGreaterThan(0)
+    expect(set.size).toBe(2)
+    expect(Array.from(set.values())[1]).toBe("bg-[url('https://xxx.webp')]")
+    //
+  })
 })
