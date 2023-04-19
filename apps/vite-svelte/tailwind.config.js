9181c1bf3e0678cfabcfc6801332627a065bd05b
sonofmagic <1324318532@qq.com>
2023-04-19 23:21:59 +0800

    chore: vite-svelte case commit

---
+++ apps/vite-svelte/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
--- apps/vite-svelte/vite.config.ts
+++ apps/vite-svelte/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import { vitePlugin as utwm } from 'unplugin-tailwindcss-mangle'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte(), utwm()]
 })
