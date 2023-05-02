f2d7ef37e773a69aa1e69c08718cf6ff8927313e
ice breaker <1324318532@qq.com>
2023-05-03 00:49:01 +0800

    feat: commit webpack hmr case

--- apps/webpack5-vue3/src/App.vue
+++ apps/webpack5-vue3/src/App.vue
@@ -4,7 +4,7 @@
       <p
         class="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Get started by editing&nbsp;
-        <code class="font-mono font-bold">pages/index.tsx</code>
+        <code class="font-mono font-bold text-[#543] bg-[url('//www.xxx.com/img/hero-pattern.svg')]">pages/index.tsx</code>
       </p>
       <div
         class="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
