85f12c73448fa28bf150f96cdbeb9ab66ccdec75
sonofmagic <1324318532@qq.com>
2023-05-02 22:29:06 +0800

    feat: eval script mangle

--- packages/unplugin-tailwindcss-mangle/src/js/index.ts
+++ packages/unplugin-tailwindcss-mangle/src/js/index.ts
@@ -1,5 +1,6 @@
-import type { StringLiteral, TemplateElement } from '@babel/types'
-import { transformSync, type BabelFileResult, type NodePath } from '@babel/core'
+import type { StringLiteral, TemplateElement, CallExpression } from '@babel/types'
+import * as t from '@babel/types'
+import { transformSync, type BabelFileResult, type NodePath, parseSync } from '@babel/core'
 import type { IHandlerOptions } from '../types'
 import { escapeStringRegexp } from '../utils'
 import { splitCode } from './split'
@@ -47,6 +48,20 @@ export function jsHandler(rawSource: string, options: IHandlerOptions) {
               enter(p: NodePath<TemplateElement>) {
                 const n = p.node
                 n.value.raw = handleValue(n.value.raw, n, options)
+              }
+            },
+            CallExpression: {
+              enter(p: NodePath<CallExpression>) {
+                const n = p.node
+                // eval()
+                if (t.isIdentifier(n.callee) && n.callee.name === 'eval') {
+                  if (t.isStringLiteral(n.arguments[0])) {
+                    const res = jsHandler(n.arguments[0].value, options)
+                    if (res.code) {
+                      n.arguments[0].value = res.code
+                    }
+                  }
+                }
               }
             }
             // noScope: true
