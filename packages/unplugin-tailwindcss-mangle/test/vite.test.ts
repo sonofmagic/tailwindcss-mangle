aaccc110edbe71722fbf4421bc871b660d4c9885
ice breaker <1324318532@qq.com>
2023-06-12 00:03:57 +0800

    chore: prepare test cases

---
+++ packages/unplugin-tailwindcss-mangle/test/vite.test.ts
@@ -0,0 +1,4 @@
+import { build } from 'vite'
+import { describe, it, expect } from 'vitest'
+
+describe('vite build', () => {})
---
+++ packages/unplugin-tailwindcss-mangle/test/webpack.test.ts
@@ -0,0 +1,11 @@
+import webpack from 'webpack'
+import { describe, it, expect } from 'vitest'
+
+describe('webpack build', () => {
+  // webpack({}, (err, stats) => {
+  //   if (err || stats.hasErrors()) {
+  //     // ...
+  //   }
+  //   // Done processing
+  // });
+})
