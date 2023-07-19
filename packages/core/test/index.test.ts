f4c089581241311155cedecef9e75715a81777b0
ice breaker <1324318532@qq.com>
2023-07-19 20:59:17 +0800

    chore(deps): upgrade

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,11 +1,11 @@
 import { getCss, getTestCase } from './utils'
 
 describe('common usage', () => {
-  it('hello-world', () => {
-    expect(getCss(getTestCase('hello-world.html'))).toMatchSnapshot()
+  it('hello-world', async () => {
+    expect(await getCss(getTestCase('hello-world.html'))).toMatchSnapshot()
   })
 
-  it('hello-world with js', () => {
-    expect(getCss([getTestCase('hello-world.html'), getTestCase('hello-world.js')])).toMatchSnapshot()
+  it('hello-world with js', async () => {
+    expect(await getCss([getTestCase('hello-world.html'), getTestCase('hello-world.js')])).toMatchSnapshot()
   })
 })
