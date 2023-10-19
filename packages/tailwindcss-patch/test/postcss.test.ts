f079033753311299a3cf712a45bf5104e7dcb545
ice breaker <1324318532@qq.com>
2023-10-20 01:15:53 +0800

    chore(deps): upgrade

--- packages/tailwindcss-patch/test/postcss.test.ts
+++ packages/tailwindcss-patch/test/postcss.test.ts
@@ -1,4 +1,6 @@
 import path from 'node:path'
+import postcss from 'postcss'
+import type { Config } from 'tailwindcss'
 import { appRoot } from './utils'
 import { processTailwindcss } from '@/core/postcss'
 import { TailwindcssPatcher } from '@/core/tw-patcher'
@@ -17,5 +19,39 @@ describe('postcss', () => {
       removeUniversalSelector: false
     })
     expect(set.size).toBe(4)
+  })
+
+  it('rpx case 0', async () => {
+    const config: Config = {
+      content: [
+        'p-[0.32rpx]',
+        'm-[23.43rpx]',
+        'space-y-[12.0rpx]',
+        'w-[12rpx]',
+        'min-w-[12rpx]',
+        'max-w-[12rpx]',
+        'h-[12rpx]',
+        'min-h-[12rpx]',
+        'max-h-[12rpx]',
+        'basis-[32rpx]',
+        'text-[32rpx]'
+      ].map((x) => {
+        return {
+          raw: x
+        }
+      }),
+      corePlugins: {
+        preflight: false
+      }
+    }
+    const { css } = await postcss([
+      require('tailwindcss')({
+        config
+      })
+    ]).process('@tailwind base;@tailwind components;@tailwind utilities;', {
+      from: undefined
+    })
+
+    expect(css).toMatchSnapshot()
   })
 })
