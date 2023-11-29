a453c22480bba61296e66fb68212a7879aa38c90
ice breaker <1324318532@qq.com>
2023-11-29 13:11:07 +0800

    test: add ice- prefix case

--- packages/unplugin-tailwindcss-mangle/test/vite.test.ts
+++ packages/unplugin-tailwindcss-mangle/test/vite.test.ts
@@ -40,4 +40,43 @@ describe('vite build', () => {
       expect(output[2].source).toMatchSnapshot()
     }
   })
+
+  it('common build change class prefix', async () => {
+    const res = (await build({
+      root: appRoot,
+      build: {
+        write: false,
+        cssMinify: false,
+        rollupOptions: {
+          output: {
+            entryFileNames: `[name].js`,
+            chunkFileNames: `[name].js`,
+            assetFileNames: `[name].[ext]`
+          }
+        }
+      },
+      plugins: [
+        utwm({
+          classListPath: path.resolve(appRoot, '.tw-patch/tw-class-list.json'),
+          classGenerator: {
+            classPrefix: 'ice-'
+          }
+        })
+      ]
+    })) as RollupOutput
+    const output = res.output
+    expect(output.length).toBe(3)
+    const jsFile = output[0]
+    expect(jsFile.type).toBe('chunk')
+    expect(jsFile.code).toMatchSnapshot()
+    expect(jsFile.code).toContain('ease-out')
+    expect(output[1].type).toBe('asset')
+    if (output[1].type === 'asset') {
+      expect(output[1].source).toMatchSnapshot()
+    }
+    expect(output[2].type).toBe('asset')
+    if (output[2].type === 'asset') {
+      expect(output[2].source).toMatchSnapshot()
+    }
+  })
 })
