e1354a26bbc45272835f05c907b87d8f02189dd2
ice breaker <1324318532@qq.com>
2025-03-02 02:56:18 +0800

    feat: add --css [file] for tailwindcss@4 extract

--- packages/tailwindcss-patch/src/cli.ts
+++ packages/tailwindcss-patch/src/cli.ts
@@ -1,3 +1,4 @@
+import type { PatchUserConfig } from '@tailwindcss-mangle/config'
 import process from 'node:process'
 import { CONFIG_NAME, getConfig, initConfig } from '@tailwindcss-mangle/config'
 import { defuOverrideArray } from '@tailwindcss-mangle/shared'
@@ -25,22 +26,33 @@ cli.command('init').action(async () => {
   logger.success(`✨ ${CONFIG_NAME}.config.ts initialized!`)
 })
 
-cli.command('extract').action(async () => {
-  const { config } = await getConfig()
-  if (config) {
-    const twPatcher = new TailwindcssPatcher(
-      {
-        patch: defuOverrideArray(config.patch!, {
-          resolve: {
-            paths: [import.meta.url],
-          },
-        }),
-      },
-    )
-    const p = await twPatcher.extract()
-    p && logger.success(`✨ tailwindcss-patch extract success! file path: ${p.filename}, classList length: ${p.classList.length}`)
-  }
-})
+cli.command('extract')
+  .option('--css [file]', 'css file entries')
+  .action(async (options) => {
+    const { config } = await getConfig()
+    const file = options.css as string
+    if (config) {
+      const twPatcher = new TailwindcssPatcher(
+        {
+          patch: defuOverrideArray(
+            config.patch!,
+            {
+              resolve: {
+                paths: [import.meta.url],
+              },
+              tailwindcss: {
+                v4: {
+                  cssEntries: file ? [file] : undefined,
+                },
+              },
+            } as Partial<PatchUserConfig>,
+          ),
+        },
+      )
+      const p = await twPatcher.extract()
+      p && logger.success(`✨ tailwindcss-patch extract success! file path: ${p.filename}, classList length: ${p.classList.length}`)
+    }
+  })
 
 cli.help()
 
