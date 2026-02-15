727021678f93f0d4b13d97872208801745172661
ice breaker <1324318532@qq.com>
2026-02-15 13:18:54 +0800

    chore: upgrade

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,17 @@
 export default {
-  '*.{js,jsx,mjs,ts,tsx,mts,vue}': [
+  '*.{js,jsx,mjs,ts,tsx,mts}': [
     'eslint --fix',
   ],
-  '*.{json,md,mdx,css,html,yml,yaml,scss}': [
+  '*.vue': [
+    'eslint --fix',
+    'stylelint --fix --allow-empty-input',
+  ],
+  '*.{json,md,mdx,html,yml,yaml}': [
     // 'prettier --with-node-modules --ignore-path .prettierignore --write',
     'eslint --fix',
+  ],
+  '*.{css,scss,sass,less}': [
+    'stylelint --fix --allow-empty-input',
   ],
   // for rust
   // '*.rs': ['cargo fmt --'],
