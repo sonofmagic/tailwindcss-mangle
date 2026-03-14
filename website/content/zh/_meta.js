9ecfd4d2172c0b7e51e78ee56edef94af7bbbd93
ice breaker <1324318532@qq.com>
2026-03-15 01:12:22 +0800

    docs(website): add patch v9 migration guide

--- website/content/zh/_meta.js
+++ website/content/zh/_meta.js
@@ -4,6 +4,7 @@ export default {
     type: 'separator',
   },
   'patch': '1.Patch',
+  'patch-v9-migration': 'Patch v8 -> v9',
   'mangle': '2.Mangle',
   'config': '3.Config',
   'integration': '4.集成实战',
